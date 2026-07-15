6601a847016a5cd6b3188714cf9b5838ad640486
purpleant1011 <purpleant1011@gmail.com>
2026-07-15 09:54:54 +0900

    fix: PlatformSession create - ip→ip_address 컬럼명 정정 (로그인 500 버그)

--- app/controllers/platform/sessions_controller.rb
+++ app/controllers/platform/sessions_controller.rb
@@ -8,7 +8,7 @@ class Platform::SessionsController < ApplicationController
     staff = PlatformStaff.find_by(email_address: raw)
     if staff && staff.authenticate(password)
       token = SecureRandom.hex(32)
-      PlatformSession.create!(platform_staff: staff, token_hash: token, ip: request.remote_ip, user_agent: request.user_agent.to_s[0, 200], last_seen_at: Time.current, expires_at: 24.hours.from_now)
+      PlatformSession.create!(platform_staff: staff, token_hash: token, ip_address: request.remote_ip, user_agent: request.user_agent.to_s[0, 200], expires_at: 24.hours.from_now)
       cookies.signed[:workmori_platform_token] = { value: token, httponly: true, expires: 24.hours.from_now, same_site: :lax, secure: Rails.env.production? }
       redirect_to platform_root_path, notice: "운영자 로그인 되었습니다."
     else
